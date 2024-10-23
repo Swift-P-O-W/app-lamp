b0c52ea69390055ade530f7fdb042986b54d426e
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2024-10-23 12:50:01 -0300

    config project
    - config uikit without storyboard

---
+++ AppLamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLamp
+//
+//  Created by Luiz Gabriel Rebelatto Bianchi and João Pedro Volponi on 23/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
